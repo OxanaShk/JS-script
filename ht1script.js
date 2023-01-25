504d7292ed95d175b01c5c9da8afa6e272d3f37c
DESKTOP-2CQ4GJ8\dell <oxana.shkraba@gmail.com>
2023-01-25 15:46:18 +0200

    added my js script

---
+++ ht1script.js
@@ -0,0 +1,52 @@
+let item1 = 5;
+console.log (item1);
+
+let item2 = 3;
+console.log (item2);
+
+let item3 = item1 + item2;
+console.log (item3);
+
+let item4 = "Yolochka";
+console.log(item4);
+
+let resPlus = item3 + item4;
+console.log(resPlus);
+
+let resMult = item3 * item4;
+console.log(resMult);
+
+let item5 = +item4;
+
+let item_6 = 15;
+let item_6_type = typeof item_6;
+console.log("item6 == " + item_6, "item_6_type == " +item_6_type);
+
+let item_7 = item_6.toString();
+console.log(typeof item_7);
+
+let item_7_type = typeof item_7;
+console.log("item_7==" + typeof item_7,  "item_7_type==" +item_7_type);
+
+let age_1 =10;
+let age_2 =18;
+let age_3 =60;
+
+if (age_1 ==10){
+    console.log("CheckAge==", age_1);
+}
+if (age_1 < age_2){
+    console.log("You don’t have access cause your age is" + age_1 + "It’s less then")
+}
+if (age_1 >=  age_2 , age_1 <  age_3){
+    console.log("Welcome!");
+}
+if (age_1 > age_3){
+    console.log("Keep calm and look Culture channel")
+}
+    else
+    {
+        console.log("Technical work");
+    }
+     
+    
